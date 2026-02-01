13cd86f92b4a9f0a96db6379cc7a8cfe5bd7d03b
alan1994bruno <alanbrunoriosmiguel@gmail.com>
2026-02-01 14:50:49 -0300

    chore: add gitignore

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from "./lib/utils";
+
+export * from "./components/ui/button";
+export * from "./components/ui/dialog";
+export * from "./components/ui/tabs";
+export * from "./components/ui/input";
+export * from "./components/ui/label";
